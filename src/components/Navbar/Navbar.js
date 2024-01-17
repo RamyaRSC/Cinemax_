3890a344cea7430db4909959131443a40a2ee21a
RamyaRSC <111015805+RamyaRSC@users.noreply.github.com>
2024-01-17 19:29:25 +0530

    second

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css'; //Import the Font Awesome CSS
 import './NavbarStyles.css';
 
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  const handleNav = () => setNav(!nav)
+
   return (
     <div className='navbar'>
       <div className='navbarContainer'>
@@ -20,13 +23,16 @@ const Navbar = () => {
           </div>
         </div>
 
-        <ul className='nav-menu'>
+        <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
             <li><a href='/home'>Home</a></li>
             <li><a href='/movie'>Movie</a></li>
             <li><a href='/about'>About</a></li>
             <li><a href='/register'>SignIn</a></li>
         </ul>
 
+        <div className="menu-bars" onClick={handleNav}>
+          <i className="fa fa-bars 2x"></i>
+        </div>
       </div>
     </div>
   )
